5dd2d5a009544bbbe6e4f8fa74d5215f0b574f89
feiniujuanjuan <1724995247@qq.com>
2024-12-04 17:33:38 +0800

    项目框架

---
+++ project/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+let count = ref('App.vue');
+
+console.log(count);
+</script>
+
+<template>
+  <h1>vue3</h1>
+</template>
+
+<style scoped></style>
